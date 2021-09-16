34f249abc0b08f33c86da93c29a017966d5b3f41
WintonTri <86459736+WintonTri@users.noreply.github.com>
2021-09-15 18:01:02 -0700

    Added XP Tracking

--- scripter_app/crafter/GemCrafter.java
+++ scripter_app/crafter/GemCrafter.java
@@ -5,8 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Optional;
 
+import org.tribot.api2007.Skills;
+import org.tribot.api2007.Skills.SKILLS;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.Ending;
 import org.tribot.script.interfaces.Painting;
 import org.tribot.script.sdk.Log;
 import org.tribot.script.sdk.Waiting;
@@ -19,11 +22,14 @@ import scripts.scripter_app.crafter.task.Cut;
 import scripts.scripter_app.crafter.task.GetCraftingItems;
 
 @ScriptManifest(authors = { "Winton" }, category = "Crafting", name = "GemCrafter")
-public class GemCrafter extends Script implements Painting {
+public class GemCrafter extends Script implements Painting, Ending {
 
 	public static boolean haveItems = true;
 	ArrayList<Node> tasks = new ArrayList<Node>(Arrays.asList(new Cut(), new GetCraftingItems()));
-
+	
+	int startingXp = Skills.getXP(SKILLS.CRAFTING);
+	int startingLevel = Skills.getActualLevel(SKILLS.CRAFTING);
+	
 	@Override
 	public void run() {
 
@@ -48,8 +54,30 @@ public class GemCrafter extends Script implements Painting {
 	@Override
 	public void onPaint(Graphics g) {
 		ArrayList<String> list = new ArrayList<String>();
+		
+		
+		list.add("Levels Gained: " + getLevelGain());
+		list.add("Xp gained: " + getXpGain());
+		
 		// TODO add name of item being crafted
 		GraphicsUtil.main(g, list);
+	}
+	
+	private int getLevelGain() {
+		int level = Skills.getActualLevel(SKILLS.CRAFTING);
+		return level - this.startingLevel;
+	}
+	
+	private int getXpGain() {
+		int xp = Skills.getXP(SKILLS.CRAFTING);
+		return xp - this.startingXp;
+	}
+
+	@Override
+	public void onEnd() {
+
+		Log.log("Gains, Levels: " + getLevelGain() + ", Xp: " + getXpGain());
+		
 	}
 
 }
